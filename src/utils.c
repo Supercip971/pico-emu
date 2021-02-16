9e8745b37aa2ac83b5f72e9d3a6b95153754d72f
Supercip971 <supercyp971@gmail.com>
2021-02-16 10:57:35 -0400

    [utils] added add_with_carry function

---
+++ src/utils.c
@@ -0,0 +1,20 @@
+#include "utils.h"
+
+uint32_t add_with_carry(uint32_t x, uint32_t y, bool carry, struct add_with_carry_result* target)
+{
+    int32_t signed_sum = (int32_t)x + (int32_t)y + (uint32_t)(carry);
+    uint32_t unsigned_sum = x + y + (uint32_t)(carry);
+    target->result = unsigned_sum & (0b01111111111111111111111111111111);
+    if((uint32_t)target->result != unsigned_sum){
+        target->carry = 1;
+    }else{
+        target->carry = 0;
+    }
+
+    if((int32_t)target->result != signed_sum){
+        target->overflow = 1;
+    }else{
+        target->overflow = 0;
+    }
+    return target->result;
+}
--- src/utils.h
+++ src/utils.h
@@ -1,6 +1,16 @@
 #ifndef UTILS_H
 #define UTILS_H
-
+#include <stdbool.h>
+#include <stdint.h>
 #define ALIGN(addr, factor) ((addr / factor) * factor)
+
+
+struct add_with_carry_result {
+    uint32_t result;
+    uint8_t carry;
+    uint8_t overflow;
+};
+
+uint32_t add_with_carry(uint32_t x, uint32_t y, bool carry, struct add_with_carry_result* target);
 
 #endif
