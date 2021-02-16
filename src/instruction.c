3dff3f3e43c34c772daa00a46bee19d191c5eb4e
Supercip971 <supercyp971@gmail.com>
2021-02-16 07:34:08 -0400

    [instruction] added ldr immediate (t1) instruction

--- src/instruction.c
+++ src/instruction.c
@@ -14,7 +14,9 @@ uint8_t run_instruction(struct pico_cpu *cpu)
 
     if((second & 0b11111000) == 0b01001000){ // LDR (literal) instruction
         return ldr_litteral_instruction(raw_instruction, cpu);
-    }    
+    }else if((second & 0b11111000) == 0b01101000){ // LDR (immediate) instruction
+        return ldr_immediate_instruction(raw_instruction, cpu);
+    }      
     printf("invalid instruction %x at %x \n ", instruction, cpu->registers.PC-2);
     return 0;
 }
--- src/instruction/ldr.h
+++ src/instruction/ldr.h
@@ -3,5 +3,5 @@
 #include "instruction_list.h"
 #include <stdint.h>
 uint8_t ldr_litteral_instruction(struct raw_instruction instruction, struct pico_cpu* cpu);
-
+uint8_t ldr_immediate_instruction(struct raw_instruction instruction , struct pico_cpu* cpu);
 #endif
