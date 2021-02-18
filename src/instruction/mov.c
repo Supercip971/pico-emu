0f589409c41e0abeb248a7032f749aedb99905f0
Supercip971 <supercyp971@gmail.com>
2021-02-18 07:40:00 -0400

    [instruction] added mov immediate instruction

--- src/instruction/mov.c
+++ src/instruction/mov.c
@@ -12,10 +12,30 @@ uint8_t mov_instruction_t1(struct raw_instruction instruction, struct pico_cpu *
     uint32_t* target = get_register(d , &cpu->registers);
     if(result == NULL || target == NULL){
         printf("invalid mov register for id %i -> %i", m, d);
-        return 1;
+        return 2;
     }
     *target = *result;
 
     printf("mov %s %s \n", get_register_name(d), get_register_name(m));
     return 0;
+}
+
+uint8_t mov_immediate(struct raw_instruction instruction, struct pico_cpu *cpu){
+    uint32_t d = (instruction.up & 0b111);
+    uint32_t imm32 = instruction.down; 
+    uint32_t* reg = get_register(d, &cpu->registers);
+    if(reg == NULL){
+
+        printf("invalid mov immediate register for id %i\n", d);
+        return 2;
+    }
+    *reg = imm32;
+
+    printf("movs %s, 0x%x \n", get_register_name(d), imm32);
+
+    cpu->registers.status.zero_condition = (imm32 == 0);
+    cpu->registers.status.negative_condition = 0; // the manual say tu put it only if the 31th bit of imm32 is set, but the 31th bit is always low... :^(
+    
+    return 0;
+
 }
