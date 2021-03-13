75f9da2eaa15113a277ccfc3fc819ef6371dfef0
supercyp <supercyp@protonmail.com>
2021-03-13 14:44:41 -0400

    [utils] added shift_c with lsls

--- src/utils.c
+++ src/utils.c
@@ -24,3 +24,54 @@ uint32_t add_with_carry(uint32_t x, uint32_t y, bool carry, struct add_with_carr
     }
     return target->result;
 }
+
+uint32_t lsl_c(uint32_t value, int amount, struct shift_c_result* res){
+    res->result = value << amount;
+    res->carry = bit_get(value,32-amount);
+    return res->result;
+}
+uint32_t lsr_c(uint32_t value, int amount, struct shift_c_result* res){
+    printf("error: lsr_c is not implemented \n");
+    return 0;
+}
+uint32_t asr_c(uint32_t value, int amount, struct shift_c_result* res){
+    printf("error: asr_c is not implemented \n");
+    return 0;
+
+}
+uint32_t ror_c(uint32_t value, int amount, struct shift_c_result* res){
+    printf("error: ror_c is not implemented \n");
+    return 0;
+
+}
+uint32_t rrx_c(uint32_t value, bool carry_in, struct shift_c_result* res){
+    printf("error: rrx_c is not implemented \n");
+    return 0;
+
+}
+
+uint32_t shift_c(uint32_t value, int shift_type, int amount, bool carry_in, struct shift_c_result* res){
+    if(amount == 0)
+    {
+        res->carry = carry_in;
+        res->result = value;
+        return value;
+    }   
+    switch (shift_type)
+    {
+    case SRType_LSL:
+        return lsl_c(value, amount, res);
+    case SRType_LSR:
+        return lsr_c(value, amount, res);
+    case SRType_ASR:
+        return asr_c(value, amount, res);
+    case SRType_ROR:
+        return ror_c(value, amount, res);
+    case SRType_RRX:
+        return rrx_c(value, amount, res);
+    
+    default:
+        printf("invalid shift_c shift type %i", shift_type);
+        return 0;
+    }
+}
--- src/utils.h
+++ src/utils.h
@@ -21,4 +21,24 @@ inline int xor (int x, int y) {
 inline uint32_t bit_get(uint32_t base_value, uint32_t offset) {
 	return (base_value >> offset) & 1;
 }
+
+enum SRType{
+    SRType_LSL = 0,
+    SRType_LSR = 1,
+    SRType_ASR = 2,
+    SRType_ROR = 3,
+    SRType_RRX = 4,
+        
+};
+struct shift_c_result {
+    uint32_t result;
+    bool carry;
+};
+uint32_t lsl_c(uint32_t value, int amount, struct shift_c_result* res);
+uint32_t lsr_c(uint32_t value, int amount, struct shift_c_result* res);
+uint32_t asr_c(uint32_t value, int amount, struct shift_c_result* res);
+uint32_t ror_c(uint32_t value, int amount, struct shift_c_result* res);
+uint32_t rrx_c(uint32_t value, bool carry_in, struct shift_c_result* res);
+
+uint32_t shift_c(uint32_t value, int shift_type, int amount, bool carry_in, struct shift_c_result* res);
 #endif
