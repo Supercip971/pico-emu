47834deb108c90c628a3780cd129030f50f11c31
https://supercip971@github.com <supercyp@protonmail.com>
2021-06-28 17:12:08 -0400

    [instruction] fix tst instruction

--- src/instruction/tst.c
+++ src/instruction/tst.c
@@ -1,4 +1,5 @@
 #include "tst.h"
+#include "../utils.h"
 #include <stdio.h>
 uint8_t tst_t1(struct raw_instruction instruction, struct pico_cpu *cpu)
 {
@@ -7,15 +8,20 @@ uint8_t tst_t1(struct raw_instruction instruction, struct pico_cpu *cpu)
 
     uint32_t *r = get_register(Rm, &cpu->registers);
     uint32_t *n = get_register(Rn, &cpu->registers);
+
     if (r == NULL || n == NULL)
     {
         printf("invalid tst register for %i & %i", Rm, Rn);
         return 2;
     }
-    uint32_t result = *r & *n;
-    int signed_r = (int)result;
-    cpu->registers.status.negative_condition = ((signed_r < 0));
+
+    struct shift_c_result res;
+    shift_c(*r, SRType_LSL, 0, cpu->registers.status.carry_flag, &res);
+
+    uint32_t result = res.result & *n;
+    cpu->registers.status.negative_condition = ((int32_t)result) < 0;
     cpu->registers.status.zero_condition = result == 0;
+    cpu->registers.status.carry_flag = res.carry;
 
     printf("tst %s, %s \n", get_register_name(Rm), get_register_name(Rn));
     return 0;
